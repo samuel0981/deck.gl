12762122fd7fac3abab74769b43fbda7534f0332
Xiaoji Chen <xiaoji@uber.com>
2019-01-19 20:06:39 -0800

    document sublayers of all composite layers

--- modules/layers/src/text-layer/text-layer.js
+++ modules/layers/src/text-layer/text-layer.js
@@ -214,7 +214,7 @@ export default class TextLayer extends CompositeLayer {
     return [
       new MultiIconLayer(
         this.getSubLayerProps({
-          id: 'text-multi-icon-layer',
+          id: 'characters',
           data,
           sdf,
           iconAtlas,
